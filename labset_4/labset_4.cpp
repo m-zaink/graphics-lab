50c71e9ff081d964b2d8ed634d506b18c9666a6e
hp <hp@hp-HP-Pro-3090-Microtower-PC.(none)>
2019-02-05 10:40:34 +0530

    Added solution to labset 4

---
+++ labset_4/labset_4.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <GL/glut.h>
+
+using namespace std;
+
+
+typedef float point[3];
+
+point v[] = {
+	{0.0, 0.0, 1.0},
+	{0.0, 1.0, 0.0},
+	{-1.0, -0.5, 0.0},
+	{1.0, -0.5, 0.0}
+};
+int n;
+
+void triangle(point a, point b, point c) {
+	glBegin(GL_POLYGON);
+	glVertex3fv(a);
+	glVertex3fv(b);
+	glVertex3fv(c);
+	glEnd();
+}
+
+void divide_triangle (point a, point b, point c, int m) {
+	point v1, v2, v3;
+	if(m > 0) {
+		for(int j = 0; j < 3; ++j)
+			v1[j] = (a[j] + b[j]) / 2;
+
+		for(int j = 0; j < 3; ++j) 
+			v2[j] = (a[j] + c[j]) / 2;
+
+		for(int j = 0; j < 3; ++j) 
+			v3[j] = (b[j] + c[j]) / 2;
+
+		divide_triangle(a, v1, v2, m - 1);
+		divide_triangle(c, v2, v3, m - 1);
+		divide_triangle(b, v3, v1, m - 1);
+	} else {
+		triangle(a, b, c);
+	}
+}
+
+void tetrahedron(int m) {
+	glColor3f(1, 0, 0);
+	divide_triangle(v[0], v[1], v[2], m);
+	glColor3f(0, 1, 0);
+	divide_triangle(v[3], v[2], v[1], m);
+	glColor3f(0, 0, 1);
+	divide_triangle(v[0], v[3], v[1], m);
+	glColor3f(0, 0, 0);
+	divide_triangle(v[0], v[2], v[3], m);
+}
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPointSize(4.0);
+	glLoadIdentity();
+	tetrahedron(n);
+	glFlush();
+}
+
+
+void myReshape(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if(w <= h) 
+		glOrtho(-2, -2, -2*(GLfloat) h / (GLfloat) w, 2*(GLfloat) h / (GLfloat) w, -10, 10);
+	else
+		glOrtho(-2*(GLfloat) h / (GLfloat) w, 2*(GLfloat) h / (GLfloat) w, -2, 2, -10, 10);
+
+	glMatrixMode(GL_MODELVIEW);
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+	cout << "Enter the number of division to be made : ";
+	cin >> n;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("3D Gasket");
+	glutDisplayFunc(display);
+	glutReshapeFunc(myReshape);
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 0);
+	glutMainLoop();
+	return 0;
+}
